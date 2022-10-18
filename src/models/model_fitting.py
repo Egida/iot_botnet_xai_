458a194087c1e1bded8291f7b8a2f35dfb81309e
kalkite <rajeshkalakoti@gmail.com>
2022-10-18 19:09:13 +0300

    dsf

---
+++ src/models/model_fitting.py
@@ -0,0 +1,31 @@
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn import metrics
+
+
+class tuning_algorithmms:
+    """
+    Tuning Algorithms
+    """
+    def __int__(self, X, y):
+        """
+        :param X:
+        :param y:
+        :return:
+        """
+        self.X = X,
+        self.y = y
+
+    def _fit_grid_random_search(self, X, y, model, parameters):
+
+        """ Training the model using Grid search or Random search hyperparameter tuning methods.
+
+        :param X: Independent Variable
+        :param y: Dependent Variable
+        :param model: Scikit learn classifier
+        :param parameters: various combinations for parameters for classifier.
+        :return:
+        """
+        with open('reports/parameter_tuning', 'a') as results:
+            results.write()
+
+        return 0
