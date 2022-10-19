c98926f7a4d4ffd84386921342825f850e4dbf9b
kalkite <rajeshkalakoti@gmail.com>
2022-10-19 15:56:09 +0300

    tuning with model parameters

--- src/models/model_fitting.py
+++ src/models/model_fitting.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pandas
 
 
-class tuning_algorithmms:
+class parameter_tuning:
     """
     Tuning Algorithms
     """
@@ -34,30 +34,50 @@ class tuning_algorithmms:
         :param search_type: Search type are 1. grid_search 2. random_search
         :return:
         """
-
+        # Classifier name
+        mlclassifier_name = str(type(ml_classifier)).split(".")[-1][:-2]
+        print("Classifier is {0}".format(mlclassifier_name))
+        # check the Parameter type,
         if search_type == 'grid_search':
-            # adding the parameters
-            hyper_param_model = GridSearchCV(ml_classifier,
-                                             param_grid=parameters,
-                                             scoring=self.metric_type,
-                                             verbose=10,
-                                             cv=self.cv)
-            hyper_param_model.fit(self.X, self.y)  # fitting the grid search.
+            # Grid Search parameter type
+            tuned_model = GridSearchCV(ml_classifier,
+                                       param_grid=parameters,
+                                       scoring=self.metric_type,
+                                       verbose=10,
+                                       cv=self.cv)
         elif search_type == 'random_search':
-            hyper_param_model = RandomizedSearchCV(estimator=ml_classifier,
-                                                     param_distributions=parameters,
-                                                     scoring=self.metric_type,
-                                                     cv=self.cv,
-                                                     verbose=4)
-            hyper_param_model.fit(self.X, self.y)
+            # Random Search Parameter Tuning
+            tuned_model = RandomizedSearchCV(estimator=ml_classifier,
+                                             param_distributions=parameters,
+                                             scoring=self.metric_type,
+                                             cv=self.cv,
+                                             verbose=10)
+
         else:
             print("===========================================")
             print(f'{search_type} is wrong key word.'
                   f'Key word should be either 1.grid_search or 2.random_search')
 
+        tuned_model = tuned_model.fit(self.X, self.y)
 
-
-        with open('reports/parameter_tuning', 'a') as results:
-            results.write('hello world')
+        with open('reports/parameter_tuning.txt', 'a') as res_logs:
+            res_logs.write('hello world')
+            res_logs.write('==' * 40)
+            res_logs.write("\n")
+            res_logs.write("1.Classifier:{0}\n".format(mlclassifier_name))
+            res_logs.write("2.Best Parameters:{0}\n".format(str(tuned_model.best_params_)))
 
         return 0
+
+
+# %%
+from sklearn.linear_model import LogisticRegression
+
+print(LogisticRegression().__class__.__name__)
+
+# %%
+classifier_name = "hellow world"
+with open('reports/parameter_tuning.txt', 'a') as results:
+    results.write('==' * 40)
+    results.write("\n")
+    results.write("1.Classifier:{0}\n".format(classifier_name))
