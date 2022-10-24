8959461ca1449fd25569f9eef13e30d7c24f6653
kalkite <rajeshkalakoti@gmail.com>
2022-10-24 12:29:13 +0300

    data reading

--- src/data/data_reading.py
+++ src/data/data_reading.py
@@ -13,15 +13,17 @@ def datareading(folder_name, file_name, class_name, sample_size, x_numbers):
     :param x_numbers: if N-BaIoT data set give 115, or if Med-IoT dataset is 100.
     :return: X(pandas.core.frame.DataFrame), y(pandas.core.frame.DataFrame).
     """
-    print("=="*40)
+    print("==" * 40)
     data = pd.read_csv(Path().joinpath(folder_name, file_name))
     class_name = data[class_name].name
-    if len(data[class_name].unique()) ==2:
+    if len(data[class_name].unique()) == 2:
         print("Binary Classification.")
     else:
         print("Multi-Class Classification.")
+    shape = data.shape
+    print('shape:{0}'.format(str(shape)))
     print(f"class_name:{class_name}")
-    print(f"class Labels:".format({data[class_name].unique()}))
+    print("class Labels:{0}".format(str(data[class_name].unique())))
     df = data.groupby(class_name).apply(lambda x: x.sample(n=sample_size)).reset_index(drop=True)
     # Encoding the labels.
     le = LabelEncoder()
@@ -32,8 +34,10 @@ def datareading(folder_name, file_name, class_name, sample_size, x_numbers):
     # data samples
     X = df.iloc[:, 0:x_numbers]
     y = df[class_name]
-    print("=="*40)
+    print("==" * 40)
     return X, y
+
+
 
 
 
