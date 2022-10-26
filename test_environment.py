53ee13e2fc0eef75880d23c1b3a7d07f8b34ce5c
kalkite <rajeshkalakoti@gmail.com>
2022-10-26 22:03:52 +0300

    test environmment

--- test_environment.py
+++ test_environment.py
@@ -1,11 +1,18 @@
+import argparse
 import sys
 from src.models.model_fitting import ModelFittingPipeLine
 from src.data.data_reading import datareading
+from src.command_line_parsing import command_line_arguments
+import joblib
+from sys import path
 
 REQUIRED_PYTHON = "python3"
 
 
 def main():
+    """
+    python version checking.
+    """
     system_major = sys.version_info.major
     if REQUIRED_PYTHON == "python":
         required_major = 2
@@ -23,11 +30,36 @@ def main():
         print(">>> Development environment passes all tests!")
 
 
+parser = command_line_arguments()
+args = parser.parse_args()
+folder_name = args.foldername
+print("folder names".format(folder_name))
 
 if __name__ == '__main__':
     main()
-    folder_name = 'data/processed'
-    X, y = datareading(folder_name=folder_name, file_name='data_file.csv', class_name='class-1', sample_size=50,
-                       x_numbers=115)
 
-    
+    directory_path = '/gpfs/mariana/home/rkalak/xai_evaluation/project/Iot_Botnet_XAI'
+
+    sys.path.insert(0, directory_path)
+
+    X, y = datareading(args.foldername, args.filename, args.class_name, sample_size=args.sample,x_numbers=args.fnumber)
+    #
+    # print(X.shape, y.shape)
+    # X, y, metric_type,
+    # feature_selection_method_name = 'fisher_score',
+    # number_of_features = 5,
+    # search_type = 'grid_search',
+    # file_location = ""
+
+    # result_path = args.rpath.strip()
+    # file_path = args.filename
+    # print("File path: {0}/{1}".format(result_path, file_path))
+
+    # print("resultant  path:{0}".format(result_path))
+    pipe_line = ModelFittingPipeLine(X, y,args.metric,args.fsname,args.fscount, args.paramtype,args.rpath)
+
+    res = pipe_line.fitting_models()
+
+
+
+    # joblib.dump(res,args.path/'model_res.pkl')
