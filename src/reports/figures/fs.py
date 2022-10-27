051c3a21cab6ef2da9e80e9bf06cc218e9926526
kalkite <rajeshkalakoti@gmail.com>
2022-10-27 18:09:42 +0300

    recent change files

---
+++ src/reports/figures/fs.py
@@ -0,0 +1,24 @@
+from src.features.build_features import Filtermethods
+import numpy as np
+
+np.arange()
+
+
+Filtermethods().fishers_score()
+
+#%%
+print("Hello world")
+print("hello world how are you")
+print("tell me something my darling sanchari")
+
+print("Hello world")
+print("hello world how are you")
+print("tell me something my darling sanchari")
+
+print("Hello world")
+print("hello world how are you")
+print("tell me something my darling sanchari")
+
+print("Hello world")
+print("hello world how are you")
+print("tell me something my darling sanchari")
