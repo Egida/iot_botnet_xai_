077ed006f957f1945d4fd11daca9db9c519c07d6
kalkite <rajeshkalakoti@gmail.com>
2022-10-30 12:43:28 +0200

    metric test

--- src/explainable_ai/metrics_test_environment.py
+++ src/explainable_ai/metrics_test_environment.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 from src.explainable_ai.xai_feature_imporance_metrics import XaiFeatureImportanceMetrics
 import lime
 import shap
@@ -10,13 +9,11 @@ def lime_monotonicity(X_test, model):
     :param X_test: (pandas.core.frame.DataFrame)
     :param model: scikit-learn model , or pre-trained model.
     """
-    print("=="*40)
+    # montonocity metric
+    print("==" * 40)
     print("\n")
     print("Lime Monotonicity")
     number_of_features = X_test.shape[1]
     X = X_test.to_numpy()
-
-
-    XaiFeatureImportanceMetrics().monotonicity_metric(model, X,)
-
+    XaiFeatureImportanceMetrics().monotonicity_metric(model, X, )
     return 0
